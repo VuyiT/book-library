303786e7bb2e34e97c64ca289f9c907416153dfb
Avuyile Tiki <avuyiletiki14@gmail.com>
2025-06-02 12:13:17 +0200

    Extract book info using map from myLibrary

--- javascript.js
+++ javascript.js
@@ -22,3 +22,27 @@ addBookToLibrary("oscar", "Jun Go", "112", "not yet read", crypto.randomUUID())
 addBookToLibrary("Billie Book", "Stacy Chan", "98", "read", crypto.randomUUID())
 
 console.log(myLibrary);
+
+// CREATIVE DIRECTION
+// Separate the diffrent headings of the book like title and author using map method
+// Create a table structire on html
+// Use js to add cells on to the table according to the user's input of books
+
+let bookTitle = myLibrary.map(book => book.title);
+console.log(bookTitle)
+
+let bookAuthor = myLibrary.map(book => book.author);
+console.log(bookAuthor)
+
+let bookPages = myLibrary.map(book => book.pages);
+console.log(bookPages)
+
+let bookRead = myLibrary.map(book => book.read);
+console.log(bookRead)
+
+let bookId = myLibrary.map(book => book.id);
+console.log(bookId)
+
+const titleCells = document.querySelector("table>tbody>tr>td[headers=title]");
+
+titleCells.textContent = myLibrary.map(book => book.title)[2];
